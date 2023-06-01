09ac88c109e7c1798d26094c466acea0687a67e3
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2023-06-01 11:21:41 +0530

    update

---
+++ App_Code/IExpro.Web.Models/SelectList.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace IExpro.Web.Models
+{
+    public class SelectedList
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+    }
+    public class ItemsList
+    {
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+    }
+
+}
--- Masters/Carpet/DefineItemCodeOther.aspx.cs
+++ Masters/Carpet/DefineItemCodeOther.aspx.cs
@@ -6,17 +6,43 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.Script.Services;
+using IExpro.Web.Models;
+using System.Web.Services;
+using System.Text.Json;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+using DocumentFormat.OpenXml.Office.Word;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Security.Cryptography.X509Certificates;
+using DocumentFormat.OpenXml.Drawing.Diagrams;
 
 public partial class Masters_Carpet_DefineItemCodeOther : System.Web.UI.Page
 {
+
+    public static int ItemFinishedId { get; set; }
+    public static int CompanyId { get; set; }
+    public static int UserId { get; set; }
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["varCompanyId"] == null)
         {
             Response.Redirect("~/Login.aspx");
-        }   
+        }
         if (!IsPostBack)
         {
+            Masters_Carpet_DefineItemCodeOther.ItemFinishedId = Request.QueryString["SrNo"] != null ? Convert.ToInt32(Request.QueryString["SrNo"]) : 0;
+            Masters_Carpet_DefineItemCodeOther.CompanyId = Session["varCompanyId"] != null ? Convert.ToInt32(Session["varCompanyId"]) : 0;
+            Masters_Carpet_DefineItemCodeOther.UserId = Session["varuserid"] != null ? Convert.ToInt32(Session["varuserid"]) : 0;
+
+            this.BindPhotoList();
+
+            this.BindCosting();
+
             SqlConnection con = new SqlConnection(ErpGlobal.DBCONNECTIONSTRING);
             con.Open();
             try
@@ -68,6 +94,8 @@ public partial class Masters_Carpet_DefineItemCodeOther : System.Web.UI.Page
         con.Open();
         try
         {
+
+
             SqlParameter[] _param = new SqlParameter[22];
             _param[0] = new SqlParameter("@ItemFinishedID", Request.QueryString["SrNo"]);
             _param[1] = new SqlParameter("@HSCODE", TxtHsCode.Text);
@@ -105,6 +133,439 @@ public partial class Masters_Carpet_DefineItemCodeOther : System.Web.UI.Page
             con.Close();
             con.Dispose();
         }
+
+    }
+
+
+
+    private void BindPhotoList()
+    {
+
+        if (Masters_Carpet_DefineItemCodeOther.ItemFinishedId > 0)
+        {
+            string query = @"Select PhotoId,ItemFinishId,PhotoName,IsPrime 
+  from tblItemPhoto Where ItemFinishId=@ItemFinishId";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+
+            using (SqlConnection con = new SqlConnection(ErpGlobal.DBCONNECTIONSTRING))
+            {
+                using (DataSet ds = SqlHelper.ExecuteDataset(con, CommandType.Text, query, parameters.ToArray()))
+                {
+                    rptPhotoList.DataSource = ds.Tables[0];
+                    rptPhotoList.DataBind();
+                }
+
+
+            }
+        }
+    }
+    protected void DeletePhoto(object sender, EventArgs e)
+    {
+        string folderPath = ConfigurationManager.AppSettings["ImagePath"];
+        int photoId = int.Parse(((sender as LinkButton).NamingContainer.FindControl("lblPhotoId") as HiddenField).Value);
+        string photoName = ((sender as LinkButton).NamingContainer.FindControl("hdnPhoto") as HiddenField).Value;
+        if (photoId > 0)
+        {
+            using (SqlConnection con = new SqlConnection(ErpGlobal.DBCONNECTIONSTRING))
+            {
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM tblItemPhoto WHERE PhotoId = @PhotoId", con))
+                {
+                    cmd.Parameters.AddWithValue("@PhotoId", photoId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    if (File.Exists(Path.Combine(folderPath, photoName)))
+                    {
+                        File.Delete(Path.Combine(folderPath, photoName));
+                    }
+                }
+                this.BindPhotoList();
+            }
+
+        }
+    }
+
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (Masters_Carpet_DefineItemCodeOther.ItemFinishedId > 0)
+        {
+
+            string folderPath = ConfigurationManager.AppSettings["ImagePath"];
+            if (PhotoImage.HasFiles)
+            {
+                int index = 1;
+                foreach (HttpPostedFile uploadedFile in PhotoImage.PostedFiles)
+                {
+                    //Check whether Directory (Folder) exists.
+                    if (!Directory.Exists(folderPath))
+                    {
+                        //If Directory (Folder) does not exists. Create it.
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    string imageName = "product-" + index.ToString() + "-F" + Masters_Carpet_DefineItemCodeOther.ItemFinishedId.ToString() + "-" + DateTime.Now.Ticks.ToString() + "-img" + Path.GetExtension(uploadedFile.FileName);
+                    string imgPath = Path.Combine(folderPath, imageName);
+                    uploadedFile.SaveAs(imgPath);
+
+                    //                    string query = @"Insert into MAIN_ITEM_IMAGE(FINISHEDID,PHOTO,MasterCompanyId)
+                    //VALUES(@ItemFinishId,@PhotoId,@CompanyId)";
+
+                    string query = @"INSERT INTO tblItemPhoto
+(PhotoName,ItemFinishId,IsPrime,IsPublished,CreatedBy,CreatedOn)
+VALUES(@PhotoName,@ItemFinishId,@IsPrime,@IsPublished,@CreatedBy,@CreatedOn)";
+
+
+                    List<SqlParameter> parameters = new List<SqlParameter>();
+                    parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+                    parameters.Add(new SqlParameter("@PhotoName", imageName));
+                    parameters.Add(new SqlParameter("@IsPrime", rdPrime.SelectedItem.Value));
+                    parameters.Add(new SqlParameter("@IsPublished", true));
+                    parameters.Add(new SqlParameter("@CreatedBy", Masters_Carpet_DefineItemCodeOther.UserId));
+                    parameters.Add(new SqlParameter("@CreatedOn", DateTime.Now));
+
+                    var result = SqlHelper.ExecuteNonQuery(ErpGlobal.DBCONNECTIONSTRING, CommandType.Text, query, parameters.ToArray());
+
+
+                }
+            }
+
+            this.BindPhotoList();
+        }
+    }
+
+
+    /// <summary>
+    /// Logs in the user
+    /// </summary>
+    /// <param name="Username">The username</param>
+    /// <param name="Password">The password</param>
+    /// <returns>true if login successful</returns>
+    [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public static string GetAttributeMaster()
+    {
+        var result = new List<SelectedList>();
+        SqlConnection dbcon = new SqlConnection(ErpGlobal.DBCONNECTIONSTRING);
+        try
+        {
+
+            string query = @"SELECT AttributeId,CompanyId,AttributeName,Description,IsPublished,CreatedBy,CreatedOn
+    FROM tblItemAttributeMaster Where CompanyId=@CompanyId";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@CompanyId", Masters_Carpet_DefineItemCodeOther.CompanyId));
+            DataSet ds = SqlHelper.ExecuteDataset(dbcon, CommandType.Text, query, parameters.ToArray());
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                result = ds.Tables[0].AsEnumerable().Select(x => new
+                SelectedList
+                {
+                    ItemId = x.Field<int>("AttributeId"),
+                    ItemName = x.Field<string>("AttributeName")
+                }).ToList();
+            }
+            var json = JsonSerializer.Serialize(result);
+
+            return json;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            dbcon.Close();
+            dbcon.Dispose();
+        }
+
+
+
+
+
+
+    }
+
+    /// <summary>
+    /// Logs in the user
+    /// </summary>
+    /// <param name="Username">The username</param>
+    /// <param name="Password">The password</param>
+    /// <returns>true if login successful</returns>
+    [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public static bool SaveData(int attributeId, string attribute)
+    {
+
+        string query = @"INSERT INTO tblItemAttributes
+(ItemFinishId,AttributeId,AttributeValue,IsPublished,CreatedBy,CreatedOn)
+VALUES(@ItemFinishId,@AttributeId,@AttributeValue,@IsPublished,@CreatedBy,@CreatedOn)";
+
+        if (!string.IsNullOrEmpty(attribute))
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+            parameters.Add(new SqlParameter("@AttributeId", attributeId));
+            parameters.Add(new SqlParameter("@AttributeValue", attribute));
+            parameters.Add(new SqlParameter("@IsPublished", true));
+            parameters.Add(new SqlParameter("@CreatedBy", Masters_Carpet_DefineItemCodeOther.UserId));
+            parameters.Add(new SqlParameter("@CreatedOn", DateTime.Now));
+
+            var result = SqlHelper.ExecuteNonQuery(ErpGlobal.DBCONNECTIONSTRING, CommandType.Text, query, parameters.ToArray());
+
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            { return false; }
+        }
+        else { return false; }
+
+    }
+
+
+
+
+
+
+    /// <summary>
+    /// Logs in the user
+    /// </summary>
+    /// <param name="Username">The username</param>
+    /// <param name="Password">The password</param>
+    /// <returns>true if login successful</returns>
+    [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public static bool DeleteProperty(int attributeId, string attribute)
+    {
+
+        string query = @"Delete From  tblItemAttributes Where ItemFinishId=@ItemFinishId and AttributeId=@AttributeId
+and AttributeValue=@AttributeValue";
+
+        if (!string.IsNullOrEmpty(attribute))
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+            parameters.Add(new SqlParameter("@AttributeId", attributeId));
+            parameters.Add(new SqlParameter("@AttributeValue", attribute));
+            var result = SqlHelper.ExecuteNonQuery(ErpGlobal.DBCONNECTIONSTRING, CommandType.Text, query, parameters.ToArray());
+
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            { return false; }
+        }
+        else { return false; }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// Logs in the user
+    /// </summary>
+    /// <param name="Username">The username</param>
+    /// <param name="Password">The password</param>
+    /// <returns>true if login successful</returns>
+    [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public static string GetItemsPropertyList()
+    {
+
+
+
+
+        string resultString = "";
+        SqlConnection dbcon = new SqlConnection(ErpGlobal.DBCONNECTIONSTRING);
+        try
+        {
+
+            string query = @"Select * from tblItemAttributeMaster x inner join tblItemAttributes y 
+on x.AttributeId=y.AttributeId Where y.ItemFinishId=@ItemFinishId 
+and x.CompanyId=@CompanyId";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+            parameters.Add(new SqlParameter("@CompanyId", Masters_Carpet_DefineItemCodeOther.CompanyId));
+            DataSet ds = SqlHelper.ExecuteDataset(dbcon, CommandType.Text, query, parameters.ToArray());
+            var result = ds.Tables[0].AsEnumerable().Select(x => new
+            {
+                AttributeId = x.Field<int>("AttributeId"),
+                AttributeName = x.Field<string>("AttributeName"),
+                AttributeValue = x.Field<string>("AttributeValue"),
+            }).ToList();
+            resultString = JsonSerializer.Serialize(result);
+            dbcon.Close();
+            dbcon.Dispose();
+
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+
+
+
+        return resultString;
+
+
+    }
+
+
+    public string GetImage(string fileName)
+    {
+        string imgDataURL = string.Empty;
+        string folderPath = ConfigurationManager.AppSettings["ImagePath"];
+        string imgPath = Path.Combine(folderPath, fileName);
+
+        if (File.Exists(imgPath))
+        {
+            byte[] byteData = System.IO.File.ReadAllBytes(imgPath);
+            string imreBase64Data = Convert.ToBase64String(byteData);
+            imgDataURL = string.Format("data:image/jpg;base64,{0}", imreBase64Data);
+
+        }
+
+
+
+        return imgDataURL;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// Logs in the user
+    /// </summary>
+    /// <param name="Username">The username</param>
+    /// <param name="Password">The password</param>
+    /// <returns>true if login successful</returns>
+    [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    public static bool SaveCosting(decimal OldPrice, decimal Price, decimal Cost, decimal Discount, decimal IsArrival, decimal IsCall)
+    {
+
+        string query = @"INSERT INTO tblItemCosting
+(ItemFinishId,CompanyId,OldPrice,Price,Cost,Discount,IsArrival,IsCall,IsPublished,CreatedBy,CreatedOn)
+VALUES
+(@ItemFinishId,@CompanyId,@OldPrice,@Price,@Cost,@Discount,@IsArrival,@IsCall,@IsPublished,@CreatedBy,@CreatedOn)
+";
+
+        List<SqlParameter> parameters = new List<SqlParameter>();
+        parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+        parameters.Add(new SqlParameter("@CompanyId", Masters_Carpet_DefineItemCodeOther.CompanyId));
+        parameters.Add(new SqlParameter("@OldPrice", OldPrice));
+        parameters.Add(new SqlParameter("@Price", Price));
+        parameters.Add(new SqlParameter("@Cost", Cost));
+        parameters.Add(new SqlParameter("@Discount", Discount));
+        parameters.Add(new SqlParameter("@IsArrival", IsArrival));
+        parameters.Add(new SqlParameter("@IsCall", IsCall));
+        parameters.Add(new SqlParameter("@IsPublished", true));
+        parameters.Add(new SqlParameter("@CreatedBy", Masters_Carpet_DefineItemCodeOther.UserId));
+        parameters.Add(new SqlParameter("@CreatedOn", DateTime.Now));
+
+        var result = SqlHelper.ExecuteNonQuery(ErpGlobal.DBCONNECTIONSTRING, CommandType.Text, query, parameters.ToArray());
+
+        if (result > 0)
+        {
+            return true;
+        }
+        else
+        { return false; }
+
+
+    }
+    protected void BindCosting()
+    {
+
+        string query = @"select * from tblItemCosting where ItemFinishId=@ItemFinishId";
+
+        List<SqlParameter> parameters = new List<SqlParameter>();
+        parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+
+        var reader = SqlHelper.ExecuteReader(ErpGlobal.DBCONNECTIONSTRING, CommandType.Text, query, parameters.ToArray());
+
+        while (reader.Read())
+        {
+            hdnCostingId.Value = reader["CostingId"].ToString();
+            txtItemPrice.Text = string.Format("{0:C}", reader["Price"].ToString());
+            txtDiscount.Text = string.Format("{0:.##}", reader["Discount"].ToString());
+            chkArrival.Checked = (bool)reader["IsArrival"];
+            chkCall.Checked = (bool)reader["IsCall"];
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    protected void btnCosting_Click(object sender, EventArgs e)
+    {
+        string query = string.Empty;
+
+        if (string.IsNullOrEmpty(hdnCostingId.Value))
+        {
+            query = @"INSERT INTO tblItemCosting
+(ItemFinishId,CompanyId,OldPrice,Price,Cost,Discount,IsArrival,IsCall,IsPublished,CreatedBy,CreatedOn)
+VALUES
+(@ItemFinishId,@CompanyId,@OldPrice,@Price,@Cost,@Discount,@IsArrival,@IsCall,@IsPublished,@CreatedBy,@CreatedOn)
+";
+
+
+        }
+        else
+        {
+            query = @"Update tblItemCosting Set Price=@Price,Discount=@Discount,IsArrival=@IsArrival,IsCall=@IsCall Where CostingId=@CostingId";
+
+        }
+
+
+        List<SqlParameter> parameters = new List<SqlParameter>();
+
+        if (!string.IsNullOrEmpty(hdnCostingId.Value))
+        {
+            parameters.Add(new SqlParameter("@CostingId", Convert.ToInt32(hdnCostingId.Value)));
+        }
+
+
+
+        parameters.Add(new SqlParameter("@ItemFinishId", Masters_Carpet_DefineItemCodeOther.ItemFinishedId));
+        parameters.Add(new SqlParameter("@CompanyId", Masters_Carpet_DefineItemCodeOther.CompanyId));
+        parameters.Add(new SqlParameter("@OldPrice", 0.0));
+        parameters.Add(new SqlParameter("@Price", Convert.ToDecimal(txtItemPrice.Text)));
+        parameters.Add(new SqlParameter("@Cost", 0.0));
+        parameters.Add(new SqlParameter("@Discount", Convert.ToDecimal(txtDiscount.Text)));
+        parameters.Add(new SqlParameter("@IsArrival", chkArrival.Checked));
+        parameters.Add(new SqlParameter("@IsCall", chkCall.Checked));
+        parameters.Add(new SqlParameter("@IsPublished", true));
+        parameters.Add(new SqlParameter("@CreatedBy", Masters_Carpet_DefineItemCodeOther.UserId));
+        parameters.Add(new SqlParameter("@CreatedOn", DateTime.Now));
+
+        var result = SqlHelper.ExecuteNonQuery(ErpGlobal.DBCONNECTIONSTRING, CommandType.Text, query, parameters.ToArray());
+
 
     }
 }
