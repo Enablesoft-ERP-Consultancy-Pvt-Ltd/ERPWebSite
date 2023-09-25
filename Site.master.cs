b734708888dfc73e8f97d9a8f74e270b6032bbf1
rakeshup94 <rakeshup94@gmail.com>
2023-09-25 10:54:41 +0530

    update

--- Site.master.cs
+++ Site.master.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,6 +11,31 @@ public partial class Site : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            if (Session["varCompanyId"] == null)
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+            else
+            {
+                imgLogo.ImageUrl.DefaultIfEmpty();
+                if (File.Exists(Server.MapPath("~/Images/Logo/" + Session["varCompanyId"] + "_company.gif")))
+                {
+                    imgLogo.ImageUrl = "~/Images/Logo/" + Session["varCompanyId"] + "_company.gif?" + DateTime.Now.ToString("dd-MMM-yyyy");
+                }
+                LblCompanyName.Text = Session["varCompanyName"].ToString();
+                LblUserName.Text = Session["varusername"].ToString();
+            }
+        }
+    }
 
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        UtilityModule.LogOut(Convert.ToInt32(Session["varuserid"]));
+        Session["varuserid"] = null;
+        Session["varCompanyId"] = null;
+        string message = "you are successfully loggedout..";
+        Response.Redirect("~/Login.aspx?Message=" + message + "");
     }
 }
