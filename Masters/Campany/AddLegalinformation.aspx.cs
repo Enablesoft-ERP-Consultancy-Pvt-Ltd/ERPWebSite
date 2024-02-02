95e060a5fa90fe0f0d63a270c9d9730f429483c1
rakeshup94 <rakeshup94@gmail.com>
2024-02-02 16:34:18 +0530

    update

--- Masters/Campany/AddLegalinformation.aspx.cs
+++ Masters/Campany/AddLegalinformation.aspx.cs
@@ -11,7 +11,7 @@ using System.IO.Compression;
 using System.IO;
 using System.Data.SqlTypes;
 using System.Net;
-public partial class Masters_AddLegalinformation : System.Web.UI.Page
+public partial class Masters_Campany_AddLegalinformation : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
